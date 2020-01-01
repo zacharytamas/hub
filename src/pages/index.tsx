6bc8b354e749223ad7b9a6d736b4d7cf1fc08f3e
Zachary <zachary@jones.cx>
2019-12-31 23:04:10 -0500

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ const Home = () => (
     <div style={{marginTop: '5rem'}}>
       <h1 style={{fontSize: '3rem'}}>Hi, I'm Zachary.</h1>
       <p>I'm a software craftsman living in Atlanta, Georgia.</p>
-      <p>Twin dad. Married to <b>Her Highness Supreme Squirrel</b>.</p>
+      <p>Married. Twin dad. Aspie.</p>
       <SiteFooter />
     </div>
   </BaseLayout>
