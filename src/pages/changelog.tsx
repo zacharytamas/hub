ec6f93d34fbdcf0e61fc63ca777ca85375f15864
Zachary Jones <zachary@jones.cx>
2020-03-12 14:51:58 -0400

    ✨ Add Changelog page

---
+++ src/pages/changelog.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { BaseLayout } from '../components/layouts';
+import ChangeLogContent from '../content/changelog.mdx';
+
+export default () => {
+  return (
+    <BaseLayout>
+      <ChangeLogContent />
+    </BaseLayout>
+  );
+};
