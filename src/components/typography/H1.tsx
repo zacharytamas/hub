8cc29676d541d5a811c7687c7a2a6577d9a41e6a
Zachary <zachary@jones.cx>
2020-03-18 16:42:22 -0400

    Minor style tweaks (#5)
    * 💄 Misc. design tweaks
    
    * 💄 Lower font size on smaller screens
    
    * 📝 Update changelog for today

--- src/components/typography/H1.tsx
+++ src/components/typography/H1.tsx
@@ -1,7 +1,14 @@
 import styled from '@emotion/styled';
 
+import { mq } from '../../style/theme';
+
 const H1 = styled.h1`
-  font-size: 3rem;
+  font-size: 2.75rem;
+  color: var(--color-accent);
+
+  ${mq.md} {
+    font-size: 3rem;
+  }
 `;
 
 export default H1;
--- src/pages/changelog.tsx
+++ src/pages/changelog.tsx
@@ -3,10 +3,8 @@ import React from 'react';
 import { BaseLayout } from '../components/layouts';
 import ChangeLogContent from '../content/changelog.mdx';
 
-export default () => {
-  return (
-    <BaseLayout>
-      <ChangeLogContent />
-    </BaseLayout>
-  );
-};
+export default () => (
+  <BaseLayout pageTitle="Changelog">
+    <ChangeLogContent />
+  </BaseLayout>
+);
