bb1bf1689f18cc5848873ae6c81ff5cc3fa56bbf
Zachary Jones <zachary@jones.cx>
2020-03-18 16:10:02 -0400

    💄 Misc. design tweaks

--- src/components/layouts/BaseLayout.tsx
+++ src/components/layouts/BaseLayout.tsx
@@ -5,15 +5,9 @@ import Head from 'next/head';
 import { css, Global } from '@emotion/core';
 import styled from '@emotion/styled';
 
+import { mq } from '../../style/theme';
 import SiteFooter from '../SiteFooter';
 import SiteHeader from '../SiteHeader';
-
-const mq = {
-  sm: `@media (min-width: 640px)`,
-  md: `@media (min-width: 768px)`,
-  lg: `@media (min-width: 1024px)`,
-  xl: `@media (min-width: 1280px)`
-};
 
 const SiteWidth = styled.section`
   max-width: 48rem;
@@ -24,63 +18,68 @@ interface BaseLayoutProps {
   pageTitle?: string;
 }
 
-const BaseLayout: React.FC<BaseLayoutProps> = ({children, pageTitle}) => {
-  return (
-    <div>
-      <Head>
-        <title>zacharytamas{pageTitle && ` | ${pageTitle}`}</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Quattrocento+Sans:wght@400;700&display=swap"
-          rel="stylesheet"
-        ></link>
-      </Head>
+const BaseLayout: React.FC<BaseLayoutProps> = ({children, pageTitle}) => (
+  <div>
+    <Head>
+      <title>zacharytamas{pageTitle && ` | ${pageTitle}`}</title>
+      <link rel="icon" href="/favicon.ico" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Quattrocento+Sans:wght@400;700&display=swap"
+        rel="stylesheet"
+      ></link>
+    </Head>
 
-      <Global
-        styles={css`
-          html,
-          body {
-            font-size: 16px;
+    <Global
+      styles={css`
+        html,
+        body {
+          font-size: 16px;
+        }
+
+        body {
+          font-family: 'Quattrocento Sans', sans-serif;
+          background-color: #fbfbfb;
+          color: #403f53;
+          padding: var(--site-edge-margin-v) var(--site-edge-margin-h);
+          margin: 0;
+          font-size: 1.125rem;
+        }
+
+        p {
+          line-height: 1.5em;
+        }
+
+        ul {
+          padding-left: 1em;
+        }
+
+        ul li {
+          line-height: 1.5em;
+        }
+
+        a {
+          color: var(--color-accent);
+        }
+
+        :root {
+          --site-edge-margin-v: 1rem;
+          --site-edge-margin-h: 2rem;
+          --color-accent: #285ba9;
+
+          ${mq.md} {
+            --site-edge-margin-v: 2rem;
+            --site-edge-margin-h: 5rem;
           }
+        }
+      `}
+    />
 
-          body {
-            font-family: 'Quattrocento Sans', sans-serif;
-            background-color: #fbfbfb;
-            color: #403f53;
-            padding: var(--site-edge-margin-v) var(--site-edge-margin-h);
-            margin: 0;
-            font-size: 1.125rem;
-          }
-
-          p {
-            line-height: 1.5em;
-          }
-
-          ul li {
-            min-height: 1.5rem;
-          }
-
-          :root {
-            --site-edge-margin-v: 1rem;
-            --site-edge-margin-h: 2rem;
-            --color-accent: #285ba9;
-
-            ${mq.md} {
-              --site-edge-margin-v: 2rem;
-              --site-edge-margin-h: 5rem;
-            }
-          }
-        `}
-      />
-
-      <SiteWidth>
-        <SiteHeader></SiteHeader>
-        {children}
-
-        <SiteFooter />
-      </SiteWidth>
-    </div>
-  );
-};
+    <SiteWidth>
+      <SiteHeader></SiteHeader>
+      {children}
+      <SiteFooter />
+    </SiteWidth>
+  </div>
+);
 
 export default BaseLayout;
