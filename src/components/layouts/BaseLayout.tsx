ecff51a83c5ab75b4ea1dfca8fc147ea15fba06c
Zachary Jones <zachary@jones.cx>
2020-03-21 14:19:46 -0400

    💄 First experiments with Tailwind

--- src/components/layouts/BaseLayout.tsx
+++ src/components/layouts/BaseLayout.tsx
@@ -2,24 +2,15 @@ import React from 'react';
 
 import Head from 'next/head';
 
-import { css, Global } from '@emotion/core';
-import styled from '@emotion/styled';
-
-import { mq } from '../../style/theme';
 import SiteFooter from '../SiteFooter';
 import SiteHeader from '../SiteHeader';
-
-const SiteWidth = styled.section`
-  max-width: 48rem;
-  margin: 0 auto;
-`;
 
 interface BaseLayoutProps {
   pageTitle?: string;
 }
 
 const BaseLayout: React.FC<BaseLayoutProps> = ({children, pageTitle}) => (
-  <div>
+  <div className="p-8">
     <Head>
       <title>zacharytamas{pageTitle && ` | ${pageTitle}`}</title>
       <link rel="icon" href="/favicon.ico" />
@@ -29,56 +20,11 @@ const BaseLayout: React.FC<BaseLayoutProps> = ({children, pageTitle}) => (
       ></link>
     </Head>
 
-    <Global
-      styles={css`
-        html,
-        body {
-          font-size: 16px;
-        }
-
-        body {
-          font-family: 'Quattrocento Sans', sans-serif;
-          background-color: #fbfbfb;
-          color: #403f53;
-          padding: var(--site-edge-margin-v) var(--site-edge-margin-h);
-          margin: 0;
-          font-size: 1.125rem;
-        }
-
-        p {
-          line-height: 1.5em;
-        }
-
-        ul {
-          padding-left: 1em;
-        }
-
-        ul li {
-          line-height: 1.5em;
-        }
-
-        a {
-          color: var(--color-accent);
-        }
-
-        :root {
-          --site-edge-margin-v: 1rem;
-          --site-edge-margin-h: 2rem;
-          --color-accent: #285ba9;
-
-          ${mq.md} {
-            --site-edge-margin-v: 2rem;
-            --site-edge-margin-h: 5rem;
-          }
-        }
-      `}
-    />
-
-    <SiteWidth>
+    <section className="container mx-auto sm:max-w-xl lg:max-w-2xl">
       <SiteHeader></SiteHeader>
       {children}
       <SiteFooter />
-    </SiteWidth>
+    </section>
   </div>
 );
 
