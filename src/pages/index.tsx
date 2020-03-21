c51126ca625fdc52e7314e361807f5e8485312e2
Zachary Jones <zachary@jones.cx>
2020-03-21 14:19:13 -0400

    Remove custom H1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 
 import { BaseLayout } from '../components/layouts';
-import { H1 } from '../components/typography';
 import About from '../content/about.mdx';
 
 const Home = () => (
   <BaseLayout>
     <div>
-      <H1>Hi, I'm Zachary.</H1>
+      <h1 className="text-blue-600">Hi, I'm Zachary.</h1>
       <About />
     </div>
   </BaseLayout>
