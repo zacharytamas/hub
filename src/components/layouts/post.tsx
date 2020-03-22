7354ffefaca23e73f21e7aae8499f08c10da2723
Zachary Jones <zachary@jones.cx>
2020-03-22 10:15:10 -0400

    ✨ Add rudimentary date support to posts

--- src/components/layouts/post.tsx
+++ src/components/layouts/post.tsx
@@ -4,12 +4,16 @@ import BaseLayout from './BaseLayout';
 
 interface Frontmatter {
   title: string;
+  date: Date;
 }
 
-export default ({title}: Frontmatter) => ({children: content}) => {
+const dateFormatter = new Intl.DateTimeFormat('all', {});
+
+export default ({title, date}: Frontmatter) => ({children: content}) => {
   return (
     <BaseLayout pageTitle={title}>
       <h1>{title}</h1>
+      <p>{dateFormatter.format(date)}</p>
       {content}
     </BaseLayout>
   );
