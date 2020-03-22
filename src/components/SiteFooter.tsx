23ff38ad14207cebd1ff60ae17b5206fd8847d25
Zachary Jones <zachary@jones.cx>
2020-03-21 20:01:43 -0400

    💄 Control how footer line breaks on small screens

--- src/components/SiteFooter.tsx
+++ src/components/SiteFooter.tsx
@@ -3,7 +3,8 @@ import React from 'react';
 export default () => (
   <footer className="mt-8 border-t-2 pt-4">
     <div className="text-gray-500 text-center">
-      &copy; {new Date().getFullYear()} Zachary Jones. All rights reserved.
+      &copy; {new Date().getFullYear()} Zachary Jones.{' '}
+      <span className="block sm:inline">All rights reserved.</span>
     </div>
   </footer>
 );
