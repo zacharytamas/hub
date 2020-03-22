283476d3f920646c679ef2c2a73342e23a704d92
Zachary Jones <zachary@jones.cx>
2020-03-22 15:26:36 -0400

    ✨ Experiments in adding a responsive menu

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -1,15 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import Link from 'next/link';
+import cn from 'classnames';
+import Link, { LinkProps } from 'next/link';
 
-const SiteHeader = () => (
-  <header className="full-bleed site-header h-12">
-    <div className="site-width flex items-center h-full">
-      <Link href="/">
-        <a className="brand font-bold no-underline">@zacharytamas</a>
-      </Link>
-    </div>
-  </header>
+const MenuItem: React.FC<LinkProps> = ({children, ...linkProps}) => (
+  <Link {...linkProps}>
+    <a className="menu-item mt-1 block px-3 -mx-3 py-2 rounded-md no-underline text-base font-medium focus:outline-none">
+      {children}
+    </a>
+  </Link>
 );
+
+const SiteHeader = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <header className="full-bleed site-header">
+      <nav>
+        <div className="site-width flex items-center justify-between h-16">
+          <div className="flex items-center">
+            <Link href="/">
+              <a className="brand font-bold no-underline">@zacharytamas</a>
+            </Link>
+          </div>
+          <div className="-mr-2 flex md:hidden">
+            <button
+              onClick={() => setOpen(o => !o)}
+              className="inline-flex items-center justify-center p-1 rounded-md text-blue-800 hover:text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 focus:text-white"
+            >
+              <svg
+                className="h-6 w-6"
+                stroke="currentColor"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  className={open ? 'hidden' : 'inline-flex'}
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                ></path>
+                <path
+                  className={!open ? 'hidden' : 'inline-flex'}
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                ></path>
+              </svg>
+            </button>
+          </div>
+        </div>
+        <div className={cn(`pb-4`, {hidden: !open})}>
+          <MenuItem href="/posts">Posts</MenuItem>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
 export default SiteHeader;
