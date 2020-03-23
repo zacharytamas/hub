4b226df4ddfd020cf9abf166d972ba2b95f9e7f9
Zachary Jones <zachary@jones.cx>
2020-03-23 11:39:01 -0400

    ♻️ Extract header icon into component

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -3,6 +3,30 @@ import React, { useState } from 'react';
 import cn from 'classnames';
 import Link, { LinkProps } from 'next/link';
 
+const MenuIcon = ({open}: {open: boolean}) => (
+  <svg
+    className="h-6 w-6"
+    stroke="currentColor"
+    fill="none"
+    viewBox="0 0 24 24"
+  >
+    <path
+      className={open ? 'hidden' : 'inline-flex'}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="M4 6h16M4 12h16M4 18h16"
+    ></path>
+    <path
+      className={!open ? 'hidden' : 'inline-flex'}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="M6 18L18 6M6 6l12 12"
+    ></path>
+  </svg>
+);
+
 const MenuItem: React.FC<LinkProps> = ({children, ...linkProps}) => (
   <Link {...linkProps}>
     <a className="menu-item mt-1 block px-3 -mx-3 py-2 rounded-md no-underline text-base font-medium focus:outline-none">
@@ -28,27 +52,7 @@ const SiteHeader = () => {
               onClick={() => setOpen(o => !o)}
               className="inline-flex items-center justify-center p-1 rounded-md text-blue-800 hover:text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 focus:text-white"
             >
-              <svg
-                className="h-6 w-6"
-                stroke="currentColor"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  className={open ? 'hidden' : 'inline-flex'}
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                ></path>
-                <path
-                  className={!open ? 'hidden' : 'inline-flex'}
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M6 18L18 6M6 6l12 12"
-                ></path>
-              </svg>
+              <MenuIcon open={open} />
             </button>
           </div>
         </div>
