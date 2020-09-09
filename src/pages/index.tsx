095e22131c5f8abba728ce908adc723a5d90a91e
Zachary Jones <zachary@jones.cx>
2020-09-09 14:53:22 -0400

    🔨 Rename layout.tsx -> Layout.tsx
    I prefer this convention.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Layout from '../components/layout';
+import Layout from '../components/Layout';
 import SEO from '../components/seo';
 
 const IndexPage = () => (
@@ -10,12 +10,16 @@ const IndexPage = () => (
 
     <p>I'm a software craftsman with 20+ years' experience living in Atlanta,&nbsp;GA.</p>
 
-    <p>I currently work at <a href="https://squareup.com/">Square</a> as a Senior Software Engineer building large-scale global payments infrastructure.</p>
+    <p>
+      I currently work at <a href="https://squareup.com/">Square</a> as a Senior Software
+      Engineer building large-scale global payments infrastructure.
+    </p>
 
-    <p>I craft digital experiences that are usually, but not always, on the Web and use modern
-tools with an emphasis on tool-building and efficiency.</p>
-
+    <p>
+      I craft digital experiences that are usually, but not always, on the Web and use
+      modern tools with an emphasis on tool-building and efficiency.
+    </p>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
