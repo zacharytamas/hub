48d18ab12175c7a409bb74d94cba049d2a73f14b
Zachary Jones <zachary@jones.cx>
2020-09-11 14:16:43 -0400

    ✨ Create EntryPreviewList component for reuse

--- src/pages/entries.tsx
+++ src/pages/entries.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import EntryPreview from '../components/entries/EntryPreview';
+import EntryPreviewList from '../components/entries/EntryPreviewList';
 import Layout from '../components/Layout';
 import useEntries from '../hooks/useEntries';
 
@@ -11,13 +11,7 @@ export default () => {
     <Layout>
       <h1>Entries</h1>
 
-      <div>
-        {entries
-          .sort((a, b) => (a.parent.modifiedTime < b.parent.modifiedTime ? 1 : -1))
-          .map((entry) => (
-            <EntryPreview key={entry.frontmatter.slug} entry={entry} />
-          ))}
-      </div>
+      <EntryPreviewList entries={entries} />
     </Layout>
   );
 };
