8ebd79cdcfbb1dcfd5e0a93dcd1b68d5dfff0ebe
Zachary Jones <zachary@jones.cx>
2020-09-11 14:19:46 -0400

    ✨ Add list of entries to front page of site
    At this moment there's kind of a confusing redundancy between `/` and `/entries` but I don't have a lot of content on this site yet so it's awkward anyway.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,39 @@
 import React from 'react';
 
+import EntryPreviewList from '../components/entries/EntryPreviewList';
 import Layout from '../components/Layout';
 import SEO from '../components/SEO';
+import useEntries from '../hooks/useEntries';
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1 className="sm:text-5xl">Hi, I'm Zachary.</h1>
+const IndexPage = () => {
+  const entries = useEntries();
 
-    <p>I'm a software craftsman with 20+ years' experience living in Atlanta,&nbsp;GA.</p>
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1 className="sm:text-5xl">Hi, I'm Zachary.</h1>
 
-    <p>
-      I currently work at <a href="https://squareup.com/">Square</a> as a Senior Software
-      Engineer building large-scale global payments infrastructure.
-    </p>
+      <p>
+        I'm a software craftsman with 20+ years' experience living in Atlanta,&nbsp;GA.
+      </p>
 
-    <p>
-      I craft digital experiences that are usually, but not always, on the Web and use
-      modern tools with an emphasis on tool-building and efficiency.
-    </p>
-  </Layout>
-);
+      <p>
+        I currently work at <a href="https://squareup.com/">Square</a> as a Senior
+        Software Engineer building large-scale global payments infrastructure.
+      </p>
+
+      <p>
+        I craft digital experiences that are usually, but not always, on the Web and use
+        modern tools with an emphasis on tool-building and efficiency.
+      </p>
+
+      <section className="border-t-2 border-gray-300 pt-8">
+        <h1>Entries</h1>
+
+        <EntryPreviewList entries={entries} />
+      </section>
+    </Layout>
+  );
+};
 
 export default IndexPage;
