167874f4d83bf956f47939083aa0516e6ec7f578
Zachary Jones <zachary@jones.cx>
2020-09-12 21:20:47 -0400

    💄 Design tweaks on LinkWithArrow

--- src/components/LinkWithArrow.tsx
+++ src/components/LinkWithArrow.tsx
@@ -1,16 +1,36 @@
 import React from 'react';
 
+import cn from 'classnames';
 import { GatsbyLinkProps, Link } from 'gatsby';
 
+type Direction = 'left' | 'right';
+
 interface LinkWithArrowProps<TState = any> extends GatsbyLinkProps<TState> {
-  arrowSide: 'left' | 'right';
+  arrowSide: Direction;
 }
 
-const LeftArrow = () => (
-  <svg className="mr-3 h-5 w-5 text-gray-500" viewBox="0 0 20 20" fill="currentColor">
+interface ArrowProps {
+  direction: Direction;
+  className?: string;
+}
+
+const Arrow = ({ direction = 'left', className }: ArrowProps) => (
+  <svg
+    className={cn(
+      `h-5 w-5`,
+      direction === 'right' ? 'ml-3' : 'mr-3',
+      direction === 'right' && 'transform -rotate-180',
+      className
+    )}
+    viewBox="0 0 20 20"
+    fill="currentColor"
+  >
     <path
       fillRule="evenodd"
-      d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z"
+      d={
+        'M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 ' +
+        '9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z'
+      }
       clipRule="evenodd"
     />
   </svg>
@@ -24,10 +44,14 @@ const LinkWithArrow: React.FC<LinkWithArrowProps> = ({
   return (
     <Link
       {...(linkProps as any)}
-      className="no-underline inline-flex items-center p-2 border rounded hover:border-gray-600 hover:font-normal transition text-gray-900"
+      className={cn(
+        `inline-flex items-center p-2`,
+        'no-underline border-2 rounded transition text-gray-900',
+        { 'flex-row-reverse': arrowSide === 'right' },
+        ['border-gray-600', 'font-normal'].map((c) => `hover:${c}`)
+      )}
     >
-      {/* TODO Make it actually support arrows on the right side. */}
-      <LeftArrow />
+      <Arrow direction={arrowSide} className="text-gray-500" />
       {children}
     </Link>
   );
