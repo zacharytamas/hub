4b6ab697adc0c5a120b5f71df2211e0b2ba239af
Zachary Jones <zachary@jones.cx>
2023-01-29 12:09:26 -0500

    tweak: Add margin to bottom of footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ interface FooterProps {}
 
 export default function Footer(_props: FooterProps) {
   return (
-    <footer className="mt-8 border-t-2 pt-4 text-sm md:text-base">
+    <footer className="my-8 border-t-2 pt-4 text-sm md:text-base">
       <span>Copyright &copy; 2008-{new Date().getFullYear()} Zachary Jones</span>
     </footer>
   )
