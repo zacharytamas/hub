9b52acdb0b3ebbbc22edbf5e7fc6496028355a18
Zachary Jones <zachary@jones.cx>
2023-01-29 00:18:38 -0500

    feat: Update <Header> and allow concept of variants for it

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,11 @@
-interface HeaderProps {}
+export interface HeaderProps {
+  variant?: 'front-page'
+}
 
-export default function Header(_props: HeaderProps) {
-  return <header className="content-container">This is my header.</header>
+export default function Header({ variant }: HeaderProps) {
+  return (
+    <header className="flex my-4">
+      <div>{variant !== 'front-page' ? <a href="/">zacharytamas</a> : null}</div>
+    </header>
+  )
 }
