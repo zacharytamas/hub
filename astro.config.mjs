bf55f03968e925afd177dd99aa589cefff1f22b5
Zachary Jones <zachary@jones.cx>
2023-02-02 17:10:32 -0500

    feat: Add TOC-related Markdown plugins and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,18 @@
 import react from '@astrojs/react'
+import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
+import remarkCollapse from 'remark-collapse'
+import remarkToc from 'remark-toc'
 
 export default defineConfig({
-  integrations: [tailwind(), react()]
-});
+  site: 'https://zacharytamas.com',
+  integrations: [tailwind(), react(), sitemap()],
+
+  markdown: {
+    remarkPlugins: [
+      [remarkToc, { tight: true, parents: ['root', 'listItem'] }],
+      [remarkCollapse, { test: 'Table of contents' }],
+    ],
+  },
+})
